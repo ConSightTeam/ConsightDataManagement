080378944b8940f4fe6c28fdd3da035e579c4ff7
Laizen Paptiti <59090008@kmitl.ac.th>
2020-02-18 21:24:58 +0700

    Add npm install to build pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const minifyCSS = require('gulp-csso');
+const install = require('gulp-install');
 const tsProject = ts.createProject('tsconfig.json');
+
+
+const PROD_DEST = 'dist';
 
 function transpile() {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest('dist'));
+        .js.pipe(gulp.dest(PROD_DEST));
 };
 
 function copyPug() {
     return gulp.src('src/views/*.pug')
-      .pipe(gulp.dest('dist/views'));
+        .pipe(gulp.dest(PROD_DEST + '/views'));
 }
 
 function css() {
     return gulp.src('src/public/stylesheets/*.css')
         .pipe(minifyCSS())
-        .pipe(gulp.dest('dist/public/stylesheets'))
+        .pipe(gulp.dest(PROD_DEST + '/public/stylesheets'))
 };
 
+function copyDependency() {
+    return gulp.src(['./package.json'])
+        .pipe(gulp.dest(PROD_DEST))
+        .pipe(install({
+            args: ['--only=production']
+        }));
+};
+
+exports.transpile = transpile;
 exports.css = css;
 exports.copyPug = copyPug;
-exports.default = gulp.parallel(transpile, copyPug, css);
+exports.copyDependency = copyDependency;
+exports.default = gulp.parallel(transpile, copyPug, css, copyDependency);
